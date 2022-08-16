92deed65d10ac9052dbf0e2d30e8baf0741efb84
sreeram54 <vaddisreeram54@gmail.com>
2022-08-16 23:37:49 +0530

    numbers

---
+++ numbers.js
@@ -0,0 +1,31 @@
+var num = 071;
+console.log(num)
+var amount = 5e-7;
+console.log(amount);
+var amount = 0.0000005;
+console.log(amount);
+var num1 = 100;// integer
+console .log(num1);
+var num2 = -100;// negative integer
+console .log(num2);
+var num3 = 10.52;// float
+console .log(num3);
+var num4 = -10.52; // negative float
+console .log(num4);
+var num5 =  256e-5;// exponential
+console .log(num5);
+var num6 =  0xfff;// hexadecimal
+console .log(num6);
+var num7 = 030;// octal 
+console .log(num7);
+var num8 = 0b0010001;//binary
+console .log(num8);
+
+
+
+
+var num1 =1+5;
+var num2 =num1/4;
+var num3 =1+5;
+var num4 =num3/4;
+console.log(num2+ ""+num4);
